7ebc3821fde5198e9f2337e13e099eaa771473bc
yuvrajsingh chouhan <Ys030198@gmail.com>
2022-08-21 12:43:51 +0530

    Updates

---
+++ precache-manifest.2ea6aa9e9cc3a324f61e205bf7eb166f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df9d14b73915179c1b04",
+    "url": "/Youtube-clone/static/css/main.446acf55.chunk.css"
+  },
+  {
+    "revision": "df9d14b73915179c1b04",
+    "url": "/Youtube-clone/static/js/main.df9d14b7.chunk.js"
+  },
+  {
+    "revision": "2bc649a667ccbd873989",
+    "url": "/Youtube-clone/static/js/1.2bc649a6.chunk.js"
+  },
+  {
+    "revision": "90ac573367a019869b79",
+    "url": "/Youtube-clone/static/js/2.90ac5733.chunk.js"
+  },
+  {
+    "revision": "ea645028320628a26524",
+    "url": "/Youtube-clone/static/js/runtime~main.ea645028.js"
+  },
+  {
+    "revision": "af110d4a9306bffcc48b208ddab58ddf",
+    "url": "/Youtube-clone/index.html"
+  }
+];
